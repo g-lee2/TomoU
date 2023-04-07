27d8c8d86893cf96717e20f6af5408cf0b94e301
Grace Lee <gracelee9901@gmail.com>
2023-04-07 02:02:28 -0700

    Add theme for consistent styling and install fonts

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,43 @@
+export const colors = {
+  brandBlue: {
+    primary: "#2182BD",
+    secondary: "#5282BD",
+    muted: "#C6DAF7",
+  },
+  brandGreen: {
+    primary: "#3F9D66",
+    secondary: "#60B28E",
+    muted: "#C7ECD9",
+  },
+  brandPurple: {
+    primary: "#7A4ABD",
+    secondary: "#9C6FD5",
+    muted: "#D4C7EC",
+  },
+  brandPink: {
+    primary: "#D74A8C",
+    secondary: "#E075A4",
+    muted: "#F1C6D8",
+  },
+  ui: {
+    primary: "#262626",
+    secondary: "#757575",
+    tertiary: "#F1F1F1",
+    quaternary: "#FFFFFF",
+    disabled: "#DEDEDE",
+    error: "#D0421B",
+    success: "#138000",
+  },
+  bg: {
+    primary: "#FFFFFF",
+    secondary: "#F1F1F1",
+  },
+  text: {
+    primary: "#262626",
+    secondary: "#757575",
+    disabled: "#9C9C9C",
+    inverse: "#FFFFFF",
+    error: "#D0421B",
+    success: "#138000",
+  },
+};
