c14967b8a6c2970a466671b98eb95b47602aa81b
Grace Lee <gracelee9901@gmail.com>
2023-05-17 20:08:31 -0700

    Add functionality to add vocabulary and definition to firestore

--- src/features/study/screens/list-details.screen.js
+++ src/features/study/screens/list-details.screen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
 import { TextInput } from "react-native-paper";
+import Icon from "react-native-vector-icons/FontAwesome";
 import { auth, db } from "../../../../firebase-config";
 import {
   collection,
@@ -8,47 +9,80 @@ import {
   getDocs,
   deleteDoc,
   doc as docs,
+  updateDoc,
+  deleteField,
+  setDoc,
 } from "firebase/firestore";
+import {
+  ListContainer,
+  ListItems,
+} from "../components/study-screen-styles.component";
 
 export const ListDetails = ({ route, navigation }) => {
-  const [newVocab, setNewVocab] = useState();
   const [listOfVocab, setListOfVocab] = useState();
-  const [isLoading, setIsLoading] = useState(true);
+  const [vocabulary, setVocabulary] = useState();
+  const [definition, setDefinition] = useState();
+  // const [isLoading, setIsLoading] = useState(true);
   const { itemId } = route.params;
 
-  const addNewVocab = async (vocab, definition) => {
-    await addDoc(collection(db, "vocabLists", itemId), {
-      vocab,
-      definition,
-      userId: auth.currentUser.uid,
+  const addNewVocab = async (vocab, def) => {
+    const docRef = docs(db, "lists", itemId);
+    await updateDoc(docRef, {
+      vocab: def,
     });
-    setNewVocab("");
+    setVocabulary("");
+    setDefinition("");
   };
 
   useEffect(() => {
     async function fetchData() {
-      const querySnapshot = await getDocs(
-        collection(db, "vocabLists").doc(itemId)
-      );
+      const querySnapshot = await getDocs(collection(db, "lists").doc(itemId));
       setListOfVocab(querySnapshot);
     }
     fetchData();
-    setIsLoading(false);
+    // setIsLoading(false);
   }, [itemId]);
 
-  // const deleteVocab = async (itemId) => {
-  //   await deleteDoc(docs(db, "lists", listId));
-  //   const updatedDocs = [...listOfVocab].filter((item) => item.id !== itemId);
+  // const deleteVocab = async (id, vocab) => {
+  //   const docRef = docs(db, "lists", id);
+  //   await updateDoc(docRef, {
+  //     vocab: deleteField(),
+  //   });
+  //   const updatedDocs = [...listOfVocab].filter(!vocab);
   //   setListOfVocab(updatedDocs);
   // };
 
+  // const renderLists = ({ item }) => {
+  //   return (
+  //     <ListContainer>
+  //       {/* <TouchableOpacity onPress={navigation.goBack()}> */}
+  //       {/* </TouchableOpacity> */}
+  //       <ListItems>{item.vocab}</ListItems>
+  //       {/* <TouchableOpacity onPress={() => deleteVocab(itemId, item)}>
+  //         <Icon name="trash-o" size={20} />
+  //       </TouchableOpacity> */}
+  //     </ListContainer>
+  //   );
+  // };
+
   return (
-    <View>
-      <TextInput placeholder="Vocabulary" />
-      <TextInput placeholder="Definition" />
-      <TouchableOpacity>
-        <Text>Add</Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      <View>
+        <TextInput
+          placeholder="Vocabulary"
+          value={vocabulary}
+          onChangeText={(v) => setVocabulary(v)}
+        />
+        <TextInput
+          placeholder="Definition"
+          value={definition}
+          onChangeText={(d) => setDefinition(d)}
+        />
+        <TouchableOpacity onPress={() => addNewVocab(vocabulary, definition)}>
+          <Text>Add</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <View>{renderLists}</View> */}
+    </>
   );
 };
